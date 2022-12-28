d183dd16f2201b738d7e7d8dc959a10cdb2c9042
Hungry J <33iamj33@gmail.com>
2022-12-27 23:30:18 -0500

    refactor the code to OOP

--- jumpWithBGMoving.js
+++ jumpWithBGMoving.js
@@ -1,140 +1,113 @@
-var context, controller, character, loop;
+var context,  loop;
 var canvasHeight = Math.floor(window.innerHeight*0.6);
 var canvasWidth = Math.floor(window.innerWidth*0.7);
 var ground = 16;
 
-
 context = document.querySelector("canvas").getContext("2d");
-
-var img = document.getElementById("characterImg");
-const characterPng = new Image();
-characterPng.src = 'soju.png';
 
 const backgroundImg= new Image();
 backgroundImg.src = 'background.png';
 
-const bg1 = new Image();
-bg1.src ="Untitled_Artwork66.png"
-const bg2 = new Image();
-bg2.src ="Untitled_Artwork67.png"
-const bg3 = new Image();
-bg3.src ="Untitled_Artwork68.png"
-const bg4 = new Image();
-bg4.src ="Untitled_Artwork71.png"
-const bg5 = new Image();
-bg5.src ="Untitled_Artwork73.png"
-
-
 context.canvas.height = canvasHeight;
 context.canvas.width = canvasWidth;
 
-//background////////////////////////////////
-const BUFFER = document.querySelector("canvas").getContext("2d");
-const TILE_SIZE = 10;
-const columns = 140;//80;//19;//parseInt(canvasWidth/TILE_SIZE);//19;
-const rows = 60;//40;//14;//parseInt(canvasHeight/TILE_SIZE);//14;
- // Each tile object has a color.
- const TILES = {
-    0: { color:'#1B1572' }, // blue
-    1: { color:'#F9F2EF' }, // star
-    2: { color:'#FFFFFF' } // star 
- }
- const PARTS = { // colums
-    // each length: 30 (30 rows)
-    // num of alphabets: 15 (15 columns)
-    A: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    B: [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    C: [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    D: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    E: [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    F: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    G: [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
-    H: [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    I: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    J: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    K: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-    L: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    M: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
-    N: [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    O: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 
-    
-    
- };
-  var all= [PARTS.A,PARTS.B,PARTS.C,PARTS.D,PARTS.E,PARTS.F,PARTS.G,PARTS.H,PARTS.I,
-    PARTS.J,PARTS.K,PARTS.L,PARTS.M,PARTS.N,PARTS.O,PARTS.O,PARTS.O,PARTS.O,PARTS.O,PARTS.O] //20
-    var blank = Array(100).fill(PARTS.O)
-    var all = all.concat(blank).concat(all) //140
- function getMultipleRandom(arr, num) {
-    
-    const shuffled = [...arr].sort(() => 0.63 - Math.random());
-  
-    return shuffled.slice(0, num);
-  }
+class Character {
+    constructor(gameWidth,gameHeight){ //gameHeight= canvasHeight
+        this.gameWidth=gameWidth;
+        this.gameHeight=gameHeight;
 
- const MAP = {
-    width:   columns * TILE_SIZE,
-    height:  rows * TILE_SIZE,
-    // This is used during image scaling to ensure the rendered image is not skewed.
-    widthHeightRatio: columns/rows, //how wide and tall the map is. scale, maintain the ratio when moving the window
-    // The values in this array correspond to the keys in the TILES object.
-    
-    //   tiles2: [[0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    //   [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-    //   [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    //   [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
-    //   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
-    tiles2: getMultipleRandom(all,100) //second parameter: how many colums
-};
+        this.height=80;
+        this.width=80;
+        this.image=document.getElementById("characterImg");
 
-function renderTiles(){  
-    var mapIndex = 0; // represents the position in the MAP.tiles array (where we  get our tile  value from)
-    // to filp the map upsidedown, just put the last index: 16*14-1 and decrement mapIdex 
-  
-    var numCol = all.length
-    
-    for (var heightIndex=0; heightIndex<MAP.tiles2.length; heightIndex+=1){
+        this.x =parseInt(gameWidth/2); //center of the canvas
+        this.y = gameHeight;
+        this.xVelocity=0; //velocity of the character
+        this.yVelocity=0;
 
-        var subArray = MAP.tiles2[heightIndex]; // sub arrays
-       // console.log("colum num: "+heightIndex + "col: "+col)
-        for (var tileIndex=0; tileIndex<subArray.length; tileIndex+=1){
-            
-            var tileValue = subArray[tileIndex]; // get tile value from the map
-            var tile = TILES[tileValue]; //get the tile obj from the  TILES
-           // console.log("tileIndex: "+ tileIndex+" value: "+ tileValue)
-            BUFFER.fillStyle= tile.color;
-            BUFFER.fillRect(heightIndex*TILE_SIZE, tileIndex*TILE_SIZE, TILE_SIZE, TILE_SIZE); 
+        this.jumping=false;
+        this.jumpCount=0;
+        this.doubleJumping= false;
+    }
+    draw(ctx){
+        if (this.doubleJumping){
+            //move the origin of matrix to center of image
+            //context.translate(character.x+character.width, character.y);
+            const time = new Date();
+            let centerW =this.x + this.width/2;
+            let centerH = this.y + this.height/2;
+            ctx.translate(centerW, centerH);
+            ctx.rotate(((2 * Math.PI) / 180) * time.getMilliseconds());
+            //time.getSeconds());
+            // + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+            //(90* Math.PI / 180);
+            ctx.translate(-centerW, -centerH);
+           }
+        ctx.drawImage(this.image, this.x, this.y,this.width, this.height);
+    }
+    update(ctrl){
+        //jumping physics (make it jump when not)
+        if (ctrl.up && this.jumping == false){
+            this.yVelocity -= 20;
+            this.jumping = true; 
+            this.image.style.transform = 'rotate(90deg)';
+            //console.log("activate jumping "+ canvasWidth+ " "+canvasHeight);
+        }
+        // double jumping
+        if (ctrl.up && this.jumping && this.jumpCount > 1 && this.doubleJumping==false){
+            this.doubleJumping = true;
+            this.yVelocity -= 20;
+            // console.log("double jumping");
         }
         
+        //left movement
+        if (ctrl.left){
+            this.xVelocity -= 0.5; // 그냥 값을 설정하는 것보다 스무스하게 움직임
+        } 
+        //right movement
+        if (ctrl.right){
+            this.xVelocity += 0.5;
+        }
+
+        //gravity
+        this.yVelocity += 1.5;
+        //add velocity to the position
+        this.x += this.xVelocity;
+        this.y += this.yVelocity;
+        //friction
+        this.xVelocity *= 0.9; //점점 멈추는것같은 없으면 엄청 스피드 업
+        this.yVelocity *= 0.9;
+
+        //collision detection
+        var groundLevel = this.gameHeight - 16 - this.height; //TODO: 16 is ground. global?
+        if (this.y > groundLevel){ //hitting the ground, falling below the ground
+            //when hitting the ground:
+            this.jumping = false;
+            this.doubleJumping = false;
+            this.y = groundLevel;
+            this.jumpCount = 0;
+            this.yVelocity = 0; // 없으면 계속 떨어지고 있어서 위로 잘 못뛰어요
+        }
+        //when leftmost
+        if(this.x < -this.width){ 
+            this.x = this.gameWidth;
+        }
+        //when rightmost
+        else if(this.x > this.gameWidth){
+            this.x = -this.width;
+        }
     }
 }
-/////////////////////////////////////////////////
 
-
-character = {
-    height:80,
-    width:80,
-
-    x:parseInt(canvasWidth/2), //center of the canvas
-    y:canvasHeight,
-    xVelocity:0, //velocity of the character
-    yVelocity:0,
-
-    jumping:false,
-    jumpCount: 0,
-    doubleJumping: false
-
-};
-
-
-controller = { //object
-    left:false,
-    right:false,
-    up:false,
-    turn:false,
-
-    //when eventlistener is 
-    keyListener: function(event){
+class Controller {
+    constructor(){
+        this.left=false;
+        this.right=false;
+        this.up=false;
+        this.turn=false;
+    }
+    keyListener(event){
         //check the key type
         var keyState = (event.type == "keydown")?true:false; //?왜 
         //var keyRelease = (event.type == "keyup")?true:false;
@@ -174,14 +147,14 @@ controller = { //object
 
         } 
     }
-};
+}
 
 class Background {
     constructor(gameWidth, gameHeight){
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.images = [bg1,bg2,bg3,bg4,bg5];
-        this.image =backgroundImg;
+        //this.images = [bg1,bg2,bg3,bg4,bg5];
+        this.image =backgroundImg;//document.getElementById("backgroundImg");//backgroundImg;
         
         this.x = 0
         this.y = 0;
@@ -202,156 +175,39 @@ class Background {
         if(this.x < 0 -this.width) this.x=0;
     }
 }
- const background = new Background(canvasWidth, canvasHeight);
-
-loop = function(){//merging the controller logic with physics
-    
-    resizeWindow();
-    //context.clearRect(0,0,window.innerWidth,window.innerHeight);
-
-    // background moving
-    background.draw(context);
-    background.update();
-
-
-    //jumping physics (make it jump when not)
-    if (controller.up && character.jumping == false){
-        character.yVelocity -= 20;
-        character.jumping = true; 
-        characterPng.style.transform = 'rotate(90deg)';
-        console.log("activate jumping "+ canvasWidth+ " "+canvasHeight);
-    }
-    // double jumping
-    if (controller.up && character.jumping && character.jumpCount > 1 && character.doubleJumping==false){
-        character.doubleJumping = true;
-        character.yVelocity -= 20;
-        console.log("double jumping");
-    }
-    
-    //left movement
-    if (controller.left){
-        character.xVelocity -= 0.5; // 그냥 값을 설정하는 것보다 스무스하게 움직임
-    } 
-    //right movement
-    if (controller.right){
-        character.xVelocity += 0.5;
-    }
-
-    // turn 360
-    if (controller.turn){
-        
-        characterPng.style.transform = 'rotate(90deg)';
-        console.log("q pressed");
-         }
-
-
-    //gravity
-    character.yVelocity += 1.5;
-    //add velocity to the position
-    character.x += character.xVelocity;
-    character.y += character.yVelocity;
-    //friction
-    character.xVelocity *= 0.9; //점점 멈추는것같은 없으면 엄청 스피드 업
-    character.yVelocity *= 0.9;
-
-    //collision detection
-    var groundLevel = canvasHeight - ground - character.height;
-    if (character.y > groundLevel){ //hitting the ground, falling below the ground
-        //when hitting the ground:
-        character.jumping = false;
-        character.doubleJumping = false;
-        character.y = groundLevel;
-        character.jumpCount = 0;
-        character.yVelocity = 0; // 없으면 계속 떨어지고 있어서 위로 잘 못뛰어요
-    }
-
-    //when leftmost
-    if(character.x < -character.width){ 
-        character.x = canvasWidth;
-    }
-    //when rightmost
-    else if(character.x > canvasWidth){
-        character.x = -character.width;
-    }
-
-    // every frame, do these:
-    
-    // paint the background 
-    // context.fillStyle = "#281e82";
-    // context.fillRect(0,0,canvasWidth,canvasHeight); //x,y,width,height
-    // context.save();
-    //renderTiles();
-    //renderDisplay();
-
-    // draw character
-   
-    //move the origin of matrix to center of image
-    //context.translate(character.x+character.width, character.y);
-    if (character.doubleJumping){
-        const time = new Date();
-        let centerW =character.x + character.width/2;
-        let centerH = character.y + character.height/2;
-        context.translate(centerW, centerH);
-        context.rotate(((2 * Math.PI) / 180) * time.getMilliseconds());
-        //time.getSeconds());
-        // + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-        //(90* Math.PI / 180);
-        context.translate(-centerW, -centerH);
-       }
-    
-    context.drawImage(characterPng, character.x, character.y,character.width, character.height);
-   context.save();
-   
-   
-   
-   context.restore();
-    /*// draw a red square
-    context.fillStyle = "#ff0000";
-    context.beginPath(); 
-    context.rect(character.x, character.y, character.width, character.height);
-    */
-    
-    /* // draw the ground (red line)
-    context.fill();
-    context.strokeStyle = "#ff0000";
-    context.lineWidth = 4;
-    context.beginPath();
-    context.moveTo(0,canvasHeight-ground);
-    context.lineTo(canvasWidth,canvasHeight-ground);
-    context.stroke(); */
-   
-    //call the loop function again when the browser is ready to draw again
-    window.requestAnimationFrame(loop);
-};
-
-function renderDisplay(){ // one call per second , 30~60fps
-    context.drawImage(BUFFER.canvas, 0,0);
-}
-
-// Set the initial width and height of the BUFFER and the DISPLAY canvases.
-BUFFER.canvas.width  = context.canvas.width  = MAP.width;
-BUFFER.canvas.height = context.canvas.height = MAP.height;
-// To ensure there is no anti-aliasing when drawing to the canvas, set image smoothing to false on both canvases.
-BUFFER.imageSmoothingEnabled = context.imageSmoothingEnabled = false;
-
 
 function resizeWindow() {
-  
     context.clearRect(0,0,window.innerWidth,window.innerHeight);
     canvasHeight =  Math.floor(window.innerHeight*0.6);
     canvasWidth = Math.floor(window.innerWidth*0.7);
-    //character.x = parseInt(canvasWidth/2);
-   // character.y = parseInt(canvasHeight-character.height);
-   
     context.canvas.height = canvasHeight;
     context.canvas.width = canvasWidth;
-
 }
+function animate(bg,char,ctrl,ctx){
+    // background moving
+    bg.draw(ctx);
+    bg.update();
+    //jumping physics (make it jump when not)
+    char.update(ctrl);
+    //draw character
+    char.draw(ctx);
+ }
+const background = new Background(canvasWidth, canvasHeight);
+const character = new Character(canvasWidth, canvasHeight);
+const controller = new Controller();
 
- // Draw the individual tiles to the buffer.
- //renderTiles();
- // Draw the BUFFER to the DISPLAY canvas.
- //renderDisplay();
+
+loop = function(){//merging the controller logic with physics
+    //context.clearRect(0,0,window.innerWidth,window.innerHeight);
+    resizeWindow();
+
+    animate(background,character,controller,context);
+    context.save(); //?
+    context.restore();
+    
+    //call the loop function again when the browser is ready to draw again
+    window.requestAnimationFrame(loop);
+};
 
 //adding eventListener to th e window(obj)
 //whenever that event is fired, exectue the controller.keyListener function
@@ -360,10 +216,8 @@ window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
 window.requestAnimationFrame(loop);
 
-
 //////////////////////////////////////////////////////////////////////
-//character selection//
-//일단 캐릭터버튼별
+//character selection//TODO
 const element = document.getElementById("char1");
 element.addEventListener("click", selectCharacter);
 
